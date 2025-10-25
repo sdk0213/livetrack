337f0a1340563b113f141a0e42450a42f33e808e
dksung <dksung@jiran.com>
2025-10-25 15:04:11 +0900

    fix: 동적 라우팅 파라미터 전달 수정
    - api/groups/[code].js에서 req.query.code 추출
    - handleGroupByCode에 code 파라미터 전달
    - code 파라미터 누락 시 400 에러 반환
    
    이전에는 code 파라미터를 전달하지 않아서
    GET/DELETE /api/groups/{code} 요청이 실패했음

--- api/groups/[code].js
+++ api/groups/[code].js
@@ -8,7 +8,15 @@ export default async function handler(req, res) {
   }
 
   try {
-    return await handleGroupByCode(req, res);
+    // Vercel의 동적 라우팅: req.query에서 code 추출
+    const { code } = req.query;
+    console.log('[groups/[code].js] Request:', req.method, 'Code:', code);
+    
+    if (!code) {
+      return res.status(400).json({ error: 'Code parameter is required' });
+    }
+    
+    return await handleGroupByCode(req, res, code);
   } catch (error) {
     console.error('Group by code error:', error);
     return res.status(500).json({ error: error.message });
